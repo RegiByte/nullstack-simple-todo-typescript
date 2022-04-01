d6e791f93d202f0b7903e50a190e3db8903d9af9
Reginaldo Junior <32272832+RegiByte@users.noreply.github.com>
2022-04-01 18:26:24 -0300

    Revert "Added basic IoC configuration"

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,10 @@
-import {NullstackClientContext} from "nullstack/types";
-import {NullstackIocContainer} from "./bootstrap/internals/NullstackIocContainer.nts";
+import { NullstackClientContext } from "nullstack/types";
 
 export interface Todo {
-    description: string
-    complete: boolean
+  description: string
+  complete: boolean
 }
 
 export interface ApplicationClientContext extends NullstackClientContext {
-    todos?: Todo[]
-    ioc: NullstackIocContainer
-    iocContainers: NullstackContainers
+  todos?: Todo[]
 }
