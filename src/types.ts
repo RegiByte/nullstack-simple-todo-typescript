2b2647f624b5aa71c6ae1f462b50dffebc2f70ec
Reginaldo Junior <reginaldo.junior696@gmail.com>
2022-04-01 18:25:53 -0300

    fixing code indent

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,13 @@
-import { NullstackClientContext } from "nullstack/types";
+import {NullstackClientContext} from "nullstack/types";
 import {NullstackIocContainer} from "./bootstrap/internals/NullstackIocContainer.nts";
 
 export interface Todo {
-  description: string
-  complete: boolean
+    description: string
+    complete: boolean
 }
 
 export interface ApplicationClientContext extends NullstackClientContext {
-  todos?: Todo[]
-  ioc: NullstackIocContainer
-  iocContainers: NullstackContainers
+    todos?: Todo[]
+    ioc: NullstackIocContainer
+    iocContainers: NullstackContainers
 }
